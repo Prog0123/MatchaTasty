5e07e30f73f287912c81762c0d18a859a885adb8
Prog0123 <riryuu12345@gmail.com>
2025-07-28 21:24:02 +0900

    商品一覧ページ作成 #16

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,11 @@ class Product < ApplicationRecord
 
   # タグ付け
   enum :category, { latte: 0, espresso: 1, matcha: 2, tea: 3 }
+
+  validates :name, presence: true
+  validates :category, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name category created_at updated_at]
+  end
 end
