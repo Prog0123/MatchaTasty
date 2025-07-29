ef2f1c26e171aacda5586cee28acc4389ad43610
Prog0123 <riryuu12345@gmail.com>
2025-07-29 23:05:14 +0900

    テストファイルの修正を行いました。

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,23 +1,49 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @product = products(:one)
+    @user = users(:one)
+  end
+
   test "should get index" do
-    get products_index_url
+    get products_path
     assert_response :success
   end
 
   test "should get show" do
-    get products_show_url
+    get product_path(@product)
     assert_response :success
   end
 
   test "should get new" do
-    get products_new_url
+    sign_in @user
+    get new_product_path
     assert_response :success
   end
 
-  test "should get create" do
-    get products_create_url
-    assert_response :success
+  test "should create product" do
+    sign_in @user
+    assert_difference("Product.count", 1) do
+      post products_path, params: {
+        product: {
+          name: "抹茶ロールケーキ",
+          category: "espresso",
+          store_name: "抹茶堂",
+          description: "濃厚な抹茶の味わい",
+          richness: 3.0,
+          bitterness: 2.5,
+          sweetness: 2.0,
+          aftertaste: 3.0,
+          appearance: 4.0,
+          total_rating: 3.0,
+          user_id: @user.id
+        }
+      }
+    end
+
+    assert_redirected_to products_path
   end
 end
