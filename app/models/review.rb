d922faca3a8db74b3188b979ac7f81f2541cd37e
Prog0123 <riryuu12345@gmail.com>
2025-07-30 22:28:56 +0900

    レビューモデルの作成 #18

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,16 @@ class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
-  validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: 1..5 }
+  def taste_level_label
+    {
+      very_light: "うすめ 🍵",
+      light: "ややうすめ 🍵🍵",
+      normal: "ふつう 🍵🍵🍵",
+      strong: "ややこいめ 🍵🍵🍵🍵",
+      very_strong: "こいめ 🍵🍵🍵🍵🍵"
+    }[taste_level.to_sym]
+  end
+
+  validates :taste_level, presence: true
+  validates :score, presence: true, numericality: { in: 0.0..5.0 }
 end
--- db/migrate/20250729124309_create_reviews.rb
+++ db/migrate/20250729124309_create_reviews.rb
@@ -3,8 +3,10 @@ class CreateReviews < ActiveRecord::Migration[7.2]
     create_table :reviews do |t|
       t.references :product, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.text :content
+      t.text :comment
       t.integer :rating
+      t.float :score, null: false
+      t.integer :taste_level, null: false, default: 0 # 味の濃さ
 
       t.timestamps
     end
