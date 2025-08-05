b4cb9b5f176cd493238f40464b5e40c24479d2c0
Prog0123 <riryuu12345@gmail.com>
2025-08-05 22:54:36 +0900

    投稿にタグの枠を追加 複数にも対応 #20

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,20 @@
 class Product < ApplicationRecord
   belongs_to :user
-
   # ActiveStorageで画像添付
   has_one_attached :image
   # 追記
   has_one :review, dependent: :destroy
   accepts_nested_attributes_for :review
+
+  # タグの関連付け
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+  attr_accessor :tag_names
+
+  # 保存時にタグの関連を更新
+  after_save :assign_tags
+
+
   # タグ付け
   enum :category, { latte: 0, espresso: 1, matcha: 2, tea: 3 }
 
@@ -20,5 +29,17 @@ class Product < ApplicationRecord
   def average_score
     return 0.0 if review.nil?
     review.score || 0.0
+  end
+
+  private
+
+  def assign_tags
+    return if tag_names.blank?
+
+    tag_list = tag_names.split(",").map(&:strip).uniq
+
+    self.tags = tag_list.map do |name|
+      Tag.find_or_create_by(name: name)
+    end
   end
 end
