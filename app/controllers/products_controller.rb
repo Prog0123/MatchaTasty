b00d195522b3d13cd0dbe7d700716a3419b0c7fb
Prog0123 <riryuu12345@gmail.com>
2025-08-07 22:34:14 +0900

    投稿の編集と削除機能の追加 #23

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   # ログインページへリダイレクト
-  before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
     @q = Product.ransack(params[:q])
@@ -14,18 +16,11 @@ class ProductsController < ApplicationController
 
   def create
     @product = current_user.products.build(product_params)
-
-    # Reviewにuserを明示的に設定
-    if @product.review
-      @product.review.user = current_user
-    end
+    @product.review.user = current_user if @product.review
 
     if @product.save
       # タグの保存処理
-      if params[:product][:tag_names]
-        tag_names = params[:product][:tag_names].split(",").map(&:strip).uniq
-        @product.tags = tag_names.map { |name| Tag.find_or_initialize_by(name:) }
-      end
+      save_tags
       redirect_to products_path, notice: "商品が登録されました。"
     else
       # エラー時はレビューオブジェクトを再構築（userも設定）
@@ -36,8 +31,25 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
     @review = @product.review  # 単数形に変更
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      save_tags
+      redirect_to product_path(@product), notice: "投稿を更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "投稿を削除しました。"
   end
 
   private
@@ -45,7 +57,21 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(
       :name, :category, :image,
-      review_attributes: [ :richness, :bitterness, :sweetness, :aftertaste, :appearance, :score, :comment, :taste_level, :user_id, :tag_names ]
+      review_attributes: [:id, :richness, :bitterness, :sweetness, :aftertaste, :appearance, :score, :comment, :taste_level]
     )
+  end
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def authorize_user!
+    redirect_to products_path, alert: "権限がありません。" unless @product.user == current_user
+  end
+
+  def save_tags
+    if params[:product][:tag_names]
+      tag_names = params[:product][:tag_names].split(",").map(&:strip).uniq
+      @product.tags = tag_names.map { |name| Tag.find_or_initialize_by(name:) }
+    end
   end
 end
